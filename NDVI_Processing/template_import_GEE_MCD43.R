2be7ce28d2e0b005c3ba1f24e34d9df8c67db1dd
Jon Page <jon.page@unsw.edu.au>
2020-07-30 12:40:39 +1000

    Queries around NDVI vs NIRv

--- NDVI_Processing/template_import_GEE_MCD43.R
+++ NDVI_Processing/template_import_GEE_MCD43.R
@@ -1,6 +1,9 @@
 # (1) Import MCD43 from an Earth Engine export
 # (2) Gapfill it with single spectrum analysis 
 # (3) Smooth it with savitzy golay 
+# 
+
+## MODIFIED TO GAPFILL NDVI - REPLACE ALL NDVI WITH NIRV TO GAPFILL NIRV INSTEAD 
 
 library(Rssa)
 library(tidyverse); library(data.table); library(dtplyr, warn.conflicts = F)
@@ -8,7 +11,7 @@ library(lubridate)
 setDTthreads(threads = 3)
 
 # Import and filter out first blank
-tmp <- read_csv("../data_general/Oz_misc_data/MCD43_OzFlux_HS_SP (2).csv") %>% 
+tmp <- read_csv("NDVI_Processing/MCD43_OzFlux_HS_SP.csv") %>% 
   filter(is.na(BRDF_Albedo_Band_Quality_Band1)==T) %>% # filter out the 'blank' image that instantiated the reducer columns
   select(-starts_with('BRDF'),-c('.geo'))
 
@@ -25,15 +28,15 @@ dat <- dat %>% as.data.table()
 
 fn_gapfill <- function(dat){
   dat <- dat %>% arrange(date) %>%
-    mutate(nirv_fill=nirv) %>% 
-    tidyr::fill(nirv_fill, .direction = 'downup')
+    mutate(ndvi_fill=ndvi) %>% 
+    tidyr::fill(ndvi_fill, .direction = 'downup')
 
   # cast to ts
-  x0 <- ts(dat$nirv, 
+  x0 <- ts(dat$ndvi, 
           start=c(year(dat$date %>% min),yday(dat$date %>% min)), 
           end=c(year(dat$date %>% max),yday(dat$date %>% max)+1),
           frequency=365)
-  x <- ts(dat$nirv_fill, 
+  x <- ts(dat$ndvi_fill, 
           start=c(year(dat$date %>% min),yday(dat$date %>% min)), 
           end=c(year(dat$date %>% max),yday(dat$date %>% max)+1),
           frequency=365)
@@ -50,7 +53,7 @@ fn_gapfill <- function(dat){
            start=c(year(dat$date %>% min),yday(dat$date %>% min)), 
            end=c(year(dat$date %>% max),yday(dat$date %>% max)),
            frequency=365)
-  dat$nirv_g <- as.numeric(x3)
+  dat$ndvi_g <- as.numeric(x3)
   return(dat)
 }
 
@@ -63,8 +66,8 @@ fn_sg <- function(dat){
   # n: window size
   # m: derivative
   dat <- dat %>% lazy_dt() %>% 
-    mutate(nirv_sg = signal::sgolayfilt(nirv_g,p=3,n=31,m=0), 
-           delta_nirv_sg = signal::sgolayfilt(nirv_g,p=3,n=31,m=1)) %>% 
+    mutate(ndvi_sg = signal::sgolayfilt(ndvi_g,p=3,n=31,m=0), 
+           delta_ndvi_sg = signal::sgolayfilt(ndvi_g,p=3,n=31,m=1)) %>% 
     as.data.table()
   return(dat)
 }
@@ -77,9 +80,14 @@ dat <- dat[,fn_sg(.SD),by=site]
 dat %>% as_tibble() %>% 
   mutate(year=year(date)) %>% 
   filter(year==2003) %>% 
-  select(nirv, nirv_g, nirv_sg, site, date) %>% 
-  gather(-site, -date, key='method',value = 'NIRV') %>% 
-  ggplot(data=., aes(date, NIRV, color=method))+
+  select(ndvi, ndvi_g, ndvi_sg, site, date) %>% 
+  gather(-site, -date, key='method',value = 'ndvi') %>% 
+  ggplot(data=., aes(date, ndvi, color=method))+
   geom_point()+
   scale_color_viridis_d(end=0.8)+
   facet_grid(method~site)
+
+
+VegIndex = dat
+
+save(VegIndex,file = "VegIndex_NDVI.Rdata")
