82896302bfdc8acdc48b0147d4b89151049ceb39
Jon Page <jon.page@unsw.edu.au>
2020-08-12 13:45:39 +1000

    Add ordering by site metric

--- MemoryPlot.R
+++ MemoryPlot.R
@@ -1,4 +1,4 @@
-MemoryPlot = function(Sites){
+MemoryPlot = function(Sites,VAR="MAP"){
 
 # This is a function which plots the various memory components of a list of 
 # sites once the R2 values have been extracted using the MemoryR2 function
@@ -13,11 +13,30 @@ MemoryPlot = function(Sites){
 #   Plot - A stacked bar chart of the memory components for each site
 # 
 
+  
+# ******************************************************************************
+# Order Sites by specified metric
+# ******************************************************************************  
+Name = names(Sites)
+MAP = unlist(lapply(Sites, function(x) x$MAP))
+MAT = unlist(lapply(Sites, function(x) x$MAT))
+LAT = unlist(lapply(Sites, function(x) x$LAT))
+  
+data = data.frame(Name,MAP,MAT,LAT)
+
+if (VAR == "MAT"){
+  data = data[order(MAT),]
+} else if (VAR == "LAT"){
+  data = data[order(LAT),]
+} else { 
+  data = data[order(MAP),]
+}
 # ******************************************************************************
 # Load model outputs  
 # ******************************************************************************
 
 Site = rep(names(Sites),3)
+Site = factor(Site, levels = data$Name)
 Model = rep(c("Current Environmental","Environmental Memory","Biological Memory"),each=length(Sites))
 Model = factor(Model,levels=c("Biological Memory","Environmental Memory","Current Environmental"))
 
@@ -36,5 +55,6 @@ Plot = ggplot(Fig,aes(fill=Model,y=Value,x=Site)) +
   scale_fill_manual(values=c("darkgreen","royalblue4","skyblue"),
                     guide = guide_legend(reverse = TRUE)) +
   theme_bw() +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  ggtitle(paste0("Memory of Sites ordered by ",VAR))
 }
