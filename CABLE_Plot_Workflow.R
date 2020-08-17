7586cfbef3e693444b8e8230973e405c06b7a57f
Jon Page <jon.page@unsw.edu.au>
2020-08-17 18:07:31 +1000

    reordered

--- CABLE_Plot_Workflow.R
+++ CABLE_Plot_Workflow.R
@@ -1,13 +1,14 @@
 source("MemoryR2.R")
 source("MemoryR2_CABLE.R")
 
+HS_CABLE = MemoryR2_CABLE("HowardSprings","HS")
+
 HS = MemoryR2("HowardSprings","HS")
 
 SP = MemoryR2("SturtPlains","SP")
 
 SP_CABLE = MemoryR2_CABLE("SturtPlains","SP")
 
-HS_CABLE = MemoryR2_CABLE("HowardSprings","HS")
 
 Sites = list("HS"=HS,"SP"=SP,"SP_CABLE"=SP_CABLE,"HS_CABLE"=HS_CABLE)
 source("MemoryPlot.R")
