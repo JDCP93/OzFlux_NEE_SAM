b3b0b846467b761dd339bb537cfc7ae296e91959
JDCP93 <jon.page@unsw.edu.au>
2020-09-28 15:42:07 +1000

    r2jags test

---
+++ NEE_workflow_AU-Cum_r2jags.R
@@ -0,0 +1,71 @@
+# NEE SAM memory exploration
+# Based on Liu et al, 2019
+# Uses the same model
+
+# Start the workflow
+rm(list = ls())
+message("Start the workflow at ",Sys.time())
+
+# load needed packages
+nee_packs <- c('rjags', 'coda', 'stats', 'R2jags', 'parallel','runjags')
+lapply(nee_packs, require, character.only = T)
+
+# runjags.options("force.summary"=T)
+
+# variables to monitor
+monitor_vars <- c("an", "ag", "phi0", "deltaXA", "weightA", "weightAP", "deltaXAP", 
+                  "cum_weightA", "cum_weightAP", "sig_y", "NEE_pred","muNEE",
+                  "ESen", 'deviance')
+
+# Load the site inputs
+load('./AU-Cum_Input.Rdata') 
+data = `AU-Cum_Input`
+inputdata = list("Nv"=data$Nv,
+                 "Ns"=data$Ns,
+                 "Nlag"=data$Nlag,
+                 "Nmem"=data$Nmem,
+                 "NlagP"=data$NlagP,
+                 "Mem_records"=data$Mem_records,
+                 "clim"=data$clim,
+                 "ppt_multiscale"=data$ppt_multiscale,
+                 "NEE"=data$NEE,
+                 "NDVI"=data$NDVI,
+                 "NblocksP"=data$NblocksP,
+                 "block"=data$block,
+                 "BlockSize"=data$BlockSize,
+                 "Nblocks"=data$Nblocks)
+
+#inputdata = dump.format(inputdata)
+
+# parallelize using runjags
+message("Begin model run at ",Sys.time())
+# run model in parallel with 6 chains and cores
+initial_results <- jags.parallel(model.file = 'NEEModel_v2.R',
+                            parameters.to.save = monitor_vars,
+                            data = inputdata,
+                            n.chains = 6, 
+                            n.burnin = 10000, 
+                            n.iter = 100000,
+                            jags.module = c('glm','dic'),
+                            n.thin = 100)
+
+# Extend the run on a single core to calculate DIC
+#message("Calculating DIC for the model at ",Sys.time())
+# results <- extend.jags(initial_results,
+#                        add.monitor=c("pd","dic"),
+#                        sample = 1000,
+#                        adapt = 200,
+#                        thin = 10,
+#                        method='rjags')
+results = c("foo")
+message("Save model output at ",Sys.time())
+# Save the results
+output = list(#"results"=results,
+              "initial_results"=initial_results)
+save(output, file=paste('NEE_output_AU-Cum_', Sys.Date(),'.Rdata', sep = ''))
+
+# Tidy up
+rm(list=ls())
+
+# Finish
+message("Finished the model run at ",Sys.time())
